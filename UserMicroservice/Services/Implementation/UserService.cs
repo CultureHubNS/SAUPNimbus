091c063ead19d222ce8a382d98c82bac70653982
kaca306 <katarina.grujic97@gmail.com>
2019-12-30 09:47:12 +0100

    Initial commit

---
+++ UserMicroservice/Services/Implementation/UserService.cs
@@ -0,0 +1,27 @@
+﻿using Commons.ExceptionHandling.Exceptions;
+using System.Collections.Generic;
+using UserMicroservice.DTO.User;
+using UserMicroservice.DTO.User.Request;
+
+namespace UserMicroservice.Services.Implementation {
+    public class UserService : IUserService {
+        public UserResponseDTO Create(CreateUserRequestDTO requestDTO) {
+            // todo
+            return new UserResponseDTO();
+        }
+
+        public List<UserResponseDTO> GetAll() {
+            // todo
+            return new List<UserResponseDTO>();
+        }
+        public UserResponseDTO GetOneByUuid(string uuid) {
+            // todo
+            throw new EntityNotFoundException($"User with uuid: {uuid} does not exist!");
+        }
+
+        public UserResponseDTO Update(UpdateUserRequestDTO requestDTO) {
+            // todo
+            return new UserResponseDTO();
+        }
+    }
+}
